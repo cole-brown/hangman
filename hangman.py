1cfcbda17fc16b261227a230cb09991f34ce5de5
Cole Brown <git@spydez.com>
2012-10-04 21:48:36 -0500

    Basic hangman.py (runs one game, always same word). Only letter guessing currently.

--- hangman.py
+++ hangman.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+# TODO
 """top level doc string"""
 
 # Python imports
@@ -7,32 +8,73 @@ import sys
 from optparse import OptionParser
 
 # local imports
+from src.HangmanGame import HangmanGame
+from src.GuessLetter import GuessLetter
+from src.GuessWord import GuessWord
+from src.FrequencyStrategy import FrequencyStrategy
 
 # CONSTANTS
+DEBUG = True # TODO - true for now
 
 #-----------------------------------------------------------------------------
-# function
+# run the game
+#-----------------------------------------------------------------------------
+def run(game, strategy):
+   """Runs one game of Hangman with the supplied strategy"""
+
+   while game.gameStatus() == HangmanGame.KEEP_GUESSING:
+      # ask strategy for a guess
+      guess = strategy.nextGuess(game)
+
+      # apply guess to game
+      guess.makeGuess(game)
+      
+      DBG(game)
+
+   return game.currentScore()
+
+#-----------------------------------------------------------------------------
+# primary function
 #-----------------------------------------------------------------------------
 def main(argv=None):
-  """this is run when the file is evaluated"""
-  if argv is None:
-    argv = sys.argv
-  try:
-    parser = OptionParser()
-#     parser.add_option("-f", "--file", dest="filename",
-#                       help="write report to FILE", metavar="FILE")
-#     parser.add_option("-q", "--quiet",
-#                       action="store_false", dest="verbose", default=True,
-#                       help="don't print status messages to stdout")
+   """this is run when the file is evaluated"""
+   if argv is None:
+      argv = sys.argv
+   try:
+      parser = OptionParser()
+#      parser.add_option("-f", "--file", dest="filename",
+#                        help="write report to FILE", metavar="FILE")
+#      parser.add_option("-q", "--quiet",
+#                        action="store_false", dest="verbose", default=True,
+#                        help="don't print status messages to stdout")
 
-    (options, args) = parser.parse_args()
+      (options, args) = parser.parse_args()
+
+      # make stuff
+      word = "factual".upper()
+      maxWrong = 5
+      game = HangmanGame(word, maxWrong)
+      strategy = FrequencyStrategy("words.txt")
+
+      # run a game!
+      run(game, strategy)
+
+      print(word + " = " + str(game.currentScore()))
+      return 0
+   except Exception as err:
+      raise err # TODO - for now, I want the stack trace
+      print(err, file=sys.stderr)
+      print("for help use --help", file=sys.stderr)
+      return 2
 
 
-    # your code here
-  except:
-    print >>sys.stderr, err.msg
-    print >>sys.stderr, "for help use --help"
-    return 2
+#-------------------------------------------------------------------------------
+# Sometimes you just want to make the voices go away...
+#-------------------------------------------------------------------------------
+def DBG(printable):
+   """No one likes bugs..."""
+   if DEBUG:
+      print(printable)
 
 #===============================================================================
 #-------------------------------------------------------------------------------
--- src/FrequencyStrategy.py
+++ src/FrequencyStrategy.py
@@ -3,8 +3,19 @@
 
 # Python imports
 import os
+import re
+import collections
 
 # local imports
+if __name__ == '__main__':
+   # this way so I can run this as main to test stuff,
+   from GuessLetter import GuessLetter
+   from GuessWord import GuessWord
+   from HangmanGame import HangmanGame
+else:
+   from src.GuessLetter import GuessLetter
+   from src.GuessWord import GuessWord
+   from src.HangmanGame import HangmanGame
 
 # CONSTANTS
 
@@ -19,7 +30,6 @@ class FrequencyStrategy:
    # Based on:
    # http://www.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
    POPULAR_LETTERS = list("ETAOINSRHDLUCMFYWGPBVKXQJZ")
-   # TODO: build this based on what letters appear in the most words?
 
    #-----------------------------------------------------------------------------
    # ctor
@@ -34,12 +44,16 @@ class FrequencyStrategy:
       # fill in allWords set
       self.parseWordsFile(filepath)
 
+      self.newGame()
+
 
    #-----------------------------------------------------------------------------
    # pick a strategy
    #-----------------------------------------------------------------------------
    def nextGuess(self, game):
-      """better description"""
+      """better description""" #TODO
+
+      self.updatePossibleWords(game)
 
       # pick a strategy
 
@@ -48,48 +62,123 @@ class FrequencyStrategy:
       # return that GuessWord/GuessLetter
 
       # but for now,
-      return GuessLetter(letterStrategy(game))
+      DBG(self.wordStrategy(game))
+      return GuessLetter(self.letterStrategy(game))
+
+
+   #-----------------------------------------------------------------------------
+   # TODO
+   #-----------------------------------------------------------------------------
+   def foo(self, game):
+      """better description""" #TODO
+      pass
+
+
+   #-----------------------------------------------------------------------------
+   # pick-a-word strategy
+   #-----------------------------------------------------------------------------
+   def wordStrategy(self, game):
+      """guess a word, based on possible words
+      return - the word to be guessed (string)"""
+
+      for word in self.possibleWords:
+         if word not in game.getIncorrectlyGuessedWords():
+            return word
 
 
    #-----------------------------------------------------------------------------
    # pick-a-letter strategy
    #-----------------------------------------------------------------------------
    def letterStrategy(self, game):
-      """guess a letter, based on letter frequency in English
+      """guess a letter, based on letter frequency in possible words
       return - the letter to be guessed (string)"""
 
-      guessed = game.getAllGuessedLetters()
+      # bulid a letter frequency list
+      letterCount = collections.Counter()
+      for word in self.possibleWords:
+         # update counter with word's letters (repeated letters removed)
+         letterCount.update(set(word))
+         # TODO test set(word) vs just word (uniq vs non-uniq'd)
 
       # pick the first letter that hasn't been guessed
-      for letter in POPULAR_LETTERS:
-         if letter not in guessed:
+      for letter, _ in letterCount.most_common():
+         if letter not in game.getAllGuessedLetters():
+            DBG("GUESS: " + letter)
             return letter
 
       # TODO: raise error if entire alphabet has been guessed already?
 
 
    #-----------------------------------------------------------------------------
+   # find possibilities
+   #-----------------------------------------------------------------------------
+   def updatePossibleWords(self, game):
+      """Uses the dictionary and current guess to narrow down the possible words"""
+
+      # turn current guess into a regex
+      current = list(game.getGuessedSoFar())
+      # match() only matches at start of string so add a final "$" to make sure
+      # it's a whole match and we're not saying "c-t" matches "catapult"
+      current.append("$") 
+#      DBG("".join(current))
+      for i in range(len(current)):
+         if current[i] == HangmanGame.MYSTERY_LETTER:
+            # convert to any-letter regex
+            current[i] = '[A-Z]{1}'
+#      DBG("".join(current))
+      guessRegex = re.compile("".join(current))
+
+      # turn incorrectly guessed letters into a regex
+      wrongLetters = game.getIncorrectlyGuessedLetters()
+      wrongRegex = None
+      if wrongLetters:
+         wrongRegex = re.compile("[" + "".join(wrongLetters) + "]+")
+
+      # TODO something for un-possible words
+
+      # need a (different) set to iterate over whist I remove words
+      # from the possibleWords set
+      tempPossibles = self.possibleWords.copy()
+
+      # test each word in the possibilites set
+#      DBG("Possibles pre: " + str(len(self.possibleWords)))
+      for word in tempPossibles:
+         # purge words that can't match current guess
+         if guessRegex.match(word) == None:
+            self.possibleWords.remove(word)
+
+         # purge words containing incorrectly guessed letters
+         elif wrongRegex != None and wrongRegex.match(word) != None:
+            self.possibleWords.remove(word)
+
+      DBG("Possibles: " + str(len(self.possibleWords)))
+
+
+   #-----------------------------------------------------------------------------
+   # reset for new game
+   #-----------------------------------------------------------------------------
+   def newGame(self):
+      """Resets class variables to be ready for another game."""
+
+      # shallow copy of dictionary set
+      self.possibleWords = self.allWords.copy()
+
+   #-----------------------------------------------------------------------------
    # Read words file
    #-----------------------------------------------------------------------------
    def parseWordsFile(self, filepath):
-      """better description"""
+      """Reads the dictionary and places each word into the allWords set.
+      Dictionary file must be one word per line.
+      exception - IOError if file can't be found/opened/read"""
 
-      # TODO catch IOError here or let bubble up?
-      #   except IOError as err:
-      #     print >>sys.stderr, "Trouble reading or opening file.\n"
-      #     print >>sys.stderr, err
-      with open(filepath, 'r') as wordsFile: 
+      with open(filepath, 'r') as dictionary: 
          DBG(pretty_size(os.path.getsize(filepath)))
 
          # read words file into set
-         for line in wordsFile:
+         for line in dictionary:
             if line.strip() != "": # avoid empty lines
-               self.allWords.add(line.strip()) # get rid of newline char
+               self.allWords.add(line.strip().upper()) # get rid of newline char
 
-# TODO NOTES!
-# read file into set
-# keep set for subsequent games
-# shallow copy (set.copy()) for each game, so can remove words as needed?
 
 #===============================================================================
 # Functions that are Cool enough not to need a class
